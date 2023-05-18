ac958eb1cc13d22d68b3de14ab08b0944aabddbe
Abdelrahman-Ali <74362044+abdoali2002@users.noreply.github.com>
2023-05-18 21:22:36 +0300

    Create UpdateBook.js

---
+++ src/pages/MangeBook/UpdateBook.js
@@ -0,0 +1,62 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
+import { Link } from "react-router-dom";
+const UpdateBook = () => {
+  return ( 
+    <div className='login-container'>
+    <h1>Update Book </h1>
+    <Form>
+    <Form.Group className="mb-3 mt-3">
+    <Form.Label>ID:</Form.Label>
+      <Form.Control type="text" placeholder="please enter book id " />
+     
+    </Form.Group>
+    <Form.Group className="mb-3"> 
+    <Form.Label>I:</Form.Label>
+    <input type="file" className="form-control" />
+    </Form.Group>
+    <Form.Group className="mb-3 " >
+    <Form.Label>Title:</Form.Label>
+      <Form.Control type="text" placeholder="please enter book title" />
+     
+    </Form.Group>
+
+    <Form.Group className="mb-3 " >
+      <Form.Label>Auther:</Form.Label>
+      <Form.Control type="text" placeholder="please enter book auther" />
+    </Form.Group>
+    <Form.Group className="mb-3 " >
+      <Form.Label htmlFor="Able" bsPrefix='pla'>Please choose catgory</Form.Label>
+      <Form.Select abled>
+        <option>Novel</option>
+        <option>Store</option>
+      </Form.Select>
+    </Form.Group>
+    <Form.Group className="mb-3 " >
+    <Form.Label>Rock Number:</Form.Label>
+      <Form.Control type="text" placeholder="please enter rock No. " />
+     
+    </Form.Group>
+   
+    <Button className='btn btn-dark w-100' variant="primary" type="submit">
+    <Link className="nav-link" to={"Book"}>submit</Link>
+      
+    </Button>
+    <Alert variant='danger' className='p-2'>
+        This is a {} alert—check it out!
+      </Alert>
+      <Alert variant='success' className='p-2'>
+        This is a {} alert—check it out!
+      </Alert>
+  </Form>
+
+  
+    
+  </div>
+   );
+}
+
+ 
+export default UpdateBook ;
