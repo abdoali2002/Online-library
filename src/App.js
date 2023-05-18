8f0c28ef54c32b749dbc8dd4cb959c5d2030c723
Abdelrahman-Ali <74362044+abdoali2002@users.noreply.github.com>
2023-05-18 21:21:40 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from './shared/Header';
+import Footer from './shared/Footer';
+import {Outlet} from "react-router-dom";
+import './style/App.css';
+import { React } from 'react'
+
+
+function App() {
+  return (
+    <div className ="App">
+      <Header />
+      <Outlet />   
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
